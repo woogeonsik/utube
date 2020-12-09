5b0920951a6218eb0c0c99cff0c57e9407c5b44f
woogeonsik1 <68350491+woogeonsik1@users.noreply.github.com>
2020-12-09 18:18:14 +0900

    GitHub Login, UserDetail complete!

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ const app = express();
 
 const CokieStore = MongoStore(session);
 
-app.use(helmet());
+//app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("static"));
