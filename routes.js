297bed3b7e9eede96c7fe79b1d2ba9ed33d8838d
woogeonsik1 <68350491+woogeonsik1@users.noreply.github.com>
2020-12-10 19:31:09 +0900

    adding comment complete

--- routes.js
+++ routes.js
@@ -30,6 +30,7 @@ const GITHUB_CALLBACK = "/auth/github/callback";
 
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -75,6 +76,7 @@ const routes = {
   me: ME,
   api: API,
   registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT,
 };
 
 export default routes;
